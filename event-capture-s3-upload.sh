bbc27f27c16ddf1b40e1987a9d51fbc14d14a580
Denis Lowe <denis.lowe@gmail.com>
2014-07-23 15:52:04 -0700

    Update event-capture-s3-upload.sh

--- event-capture-s3-upload.sh
+++ event-capture-s3-upload.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-S3_HOME=/usr/local/bin/s3cmd-1.5.0-beta1
-S3_PATH=s3://findly.development/analytics/raw-logs/event-capture
+#
+# This script should be called within the 'postrotate' section of the logrotate config file
+# This file accepts the full path to the files(s) to the archived as the parameter
+#
+
+# The log file to be copied to s3
 LOG_FILE=$1
+
+# S3 access keys
+S3_CONFIG=s3cmd.cfg
+
+# S3 root folder to store the files
+S3_PATH=s3://findly.development/analytics/raw-logs/event-capture
+
+S3_HOME=/usr/local/bin/s3cmd-1.5.0-beta1
 LOCK_FILE=/tmp/evcap.lock
 FILE_NAME=`hostname`-`date -u +%H%M%S.%s`
 
 # check for lock file
-lockfile -r 0 $LOCK_FILE
+lockfile -r 10 $LOCK_FILE
 
 if [[ $? -ne 0 ]]; then
         # send email alert
@@ -16,13 +28,11 @@ if [[ $? -ne 0 ]]; then
         exit 1
 fi
 
-cd $LOG_FILE_DIR
-
 # cat all rotated files into a single gzip file
 cat $LOG_FILE.* | gzip > $FILE_NAME.gz
 
 # upload to s3 - for hadoop integration folders must prefixed with 'date='
-$S3_HOME/s3cmd put $FILE_NAME.gz $S3_PATH/date=`date -u +%Y-%m-%d`/
+$S3_HOME/s3cmd -c $S3_CONFIG put $FILE_NAME.gz $S3_PATH/date=`date -u +%Y-%m-%d`/
 
 if [[ $? -ne 0 ]]; then
         # send email alert
